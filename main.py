175e01101f38979850c52bbc42f5eb998536d2d3
Shaun Saker <sakershaun@gmail.com>
2020-08-26 09:58:53 +0200

    separate loop and single use of main

--- main.py
+++ main.py
@@ -53,173 +53,146 @@ def removeStock(stockRef, _symbol):
     exchangeRef.set({"symbols": newExchangeSymbols}, merge=True)
 
 
-for symbolData in exchangeSymbols:
-    symbol = symbolData["symbol"]
+def processStock(symbol):
 
-    # if we're targetting a specific symbol and it matches or we're not targetting any symbols
-    if (targetSymbol and targetSymbol == symbol) or not targetSymbol:
+    # get the stock
+    stockRef = exchangeRef.collection("stocks").document(symbol)
+    stockData = stockRef.get().to_dict()
+
+    if not stockData:
+        # create new
+        if freshy:
+            stock = Stock(symbol=symbol)
+        else:
+            removeStock(stockRef, symbol)
+            return
+    else:
+        # replace any None, NaN values with 0 (lord knows how they got in there, probably my spaghetti code)
+        stockData = falsyToInt(stockData)
+
+        stock = typedload.load(stockData, Stock)
+
+    # don't process stocks that have already been updated today
+    if unprocessed and stock.lastUpdated == today:
+        return
+
+    # get the latest shares outstanding
+    sharesOutstanding = fetchSharesOutstanding(symbol)
+
+    if not sharesOutstanding:
+        print("No shares.")
+        removeStock(stockRef, symbol)
+        return
+
+    stock.sharesOutstanding = sharesOutstanding
+
+    # get the latest price
+    latestPrice = fetchLatestPrice(stock, exchange)
+
+    # if there is no price or the price is 0, remove the stock
+    if not latestPrice:
+        print("No price.")
+        removeStock(stockRef, symbol)
+        return
+
+    stock.currentPrice = latestPrice
+
+    # get the latest financial statements
+    yahooStatements = fetchLatestFinancialStatements(symbol)
+
+    # if latest statements are empty
+    if yahooStatements.incomeStatements.quarterly == {}:
+        print("No latest financial statements.")
+        removeStock(stockRef, symbol)
+        return
+
+    # parse latest statements
+    latestFinancialStatements = makeLatestFinancialStatements(yahooStatements)
+
+    # merge the existing and latest financial statements
+    if freshy:
+        # merge the historical
+        historicalFundamentals = fetchHistoricalFundamentals(symbol, exchange)
+
+        if not historicalFundamentals:
+            print("No historical financial statements.")
+            removeStock(stockRef, symbol)
+            return
+
+        historicalFinancialStatements = makeHistoricalFinancialStatements(
+            historicalFundamentals
+        )
+        financialStatements = makeFinancialStatements(
+            FinancialStatements(), historicalFinancialStatements
+        )
+
+        # merge the latest
+        financialStatements = makeFinancialStatements(
+            financialStatements, latestFinancialStatements
+        )
+
+    else:
+        financialStatements = makeFinancialStatements(
+            stock.financialStatements, latestFinancialStatements
+        )
+
+    # if empty financial statements, we don't want to save it
+    if not financialStatements:
+        print("No financial statements.")
+        removeStock(stockRef, symbol)
+        return
+
+    stock.financialStatements = financialStatements
+
+    # get new historical pricing
+    historicalPricing = fetchHistoricalPricing(symbol)
+
+    if not historicalPricing:
+        print("No historical pricing.")
+        removeStock(stockRef, symbol)
+        return
+
+    if historicalPricing:
+        stock.historicalPricing = historicalPricing
+
+    # get the latest dividends
+    stock = handleDividendsPaid(stock)
+
+    # evaluate the stock
+    stock.valuation = evaluate(stock)
+
+    # add the last updated date
+    stock.lastUpdated = today
+
+    # convert our stock class to a json string
+    stockJson = json.loads(json.dumps(stock, default=lambda o: o.__dict__, indent=2))
+
+    stockRef.set(stockJson, merge=True)
+
+    if freshy:
+        # store the data locally
+        filepath = f"data/stocks/{exchange}/{symbol}.json"
+        with safeOpenWrite(filepath) as file:
+            jsonString = json.dumps(stockJson, default=lambda o: o.__dict__, indent=2)
+            file.write(jsonString)
+
+    print(
+        f"{symbol} is {stock.valuation.health}. You should {stock.valuation.instruction}. You can expected a return of {stock.valuation.expectedReturn}%. The current price is {stock.currentPrice} and we value the stock at {stock.valuation.fairValue}."
+    )
+
+    return
+
+
+if targetSymbol:
+    symbol = targetSymbol
+    processStock(symbol)
+else:
+    for symbolData in exchangeSymbols:
+        symbol = symbolData["symbol"]
+
         symbolCount += 1
         print(
             f"Updating {exchangeName}: {symbol}, {symbolCount} of {len(exchangeSymbols)}..."
         )
 
-        # get the stock
-        stockRef = exchangeRef.collection("stocks").document(symbol)
-        stockData = stockRef.get().to_dict()
-
-        if not stockData:
-            # create new
-            if freshy:
-                stock = Stock(symbol=symbol)
-            else:
-                removeStock(stockRef, symbol)
-
-                # skip stocks that don't exist
-                if targetSymbol:
-                    break
-
-                continue
-        else:
-            # replace any None, NaN values with 0 (lord knows how they got in there, probably my spaghetti code)
-            stockData = falsyToInt(stockData)
-
-            stock = typedload.load(stockData, Stock)
-
-        # don't process stocks that have already been updated today
-        if unprocessed and stock.lastUpdated == today:
-            continue
-
-        # get the latest shares outstanding
-        sharesOutstanding = fetchSharesOutstanding(symbol)
-
-        if not sharesOutstanding:
-            print("No shares.")
-            removeStock(stockRef, symbol)
-
-            if targetSymbol:
-                break
-
-            continue
-
-        stock.sharesOutstanding = sharesOutstanding
-
-        # get the latest price
-        latestPrice = fetchLatestPrice(stock, exchange)
-
-        # if there is no price or the price is 0, remove the stock
-        if not latestPrice:
-            print("No price.")
-            removeStock(stockRef, symbol)
-
-            if targetSymbol:
-                break
-
-            continue
-
-        stock.currentPrice = latestPrice
-
-        # get the latest financial statements
-        yahooStatements = fetchLatestFinancialStatements(symbol)
-
-        # if latest statements are empty
-        if yahooStatements.incomeStatements.quarterly == {}:
-            print("No latest financial statements.")
-            removeStock(stockRef, symbol)
-
-            if targetSymbol:
-                break
-
-            continue
-
-        # parse latest statements
-        latestFinancialStatements = makeLatestFinancialStatements(yahooStatements)
-
-        # merge the existing and latest financial statements
-        if freshy:
-            # merge the historical
-            historicalFundamentals = fetchHistoricalFundamentals(symbol, exchange)
-
-            if not historicalFundamentals:
-                print("No historical financial statements.")
-                removeStock(stockRef, symbol)
-
-                if targetSymbol:
-                    break
-
-                continue
-
-            historicalFinancialStatements = makeHistoricalFinancialStatements(
-                historicalFundamentals
-            )
-            financialStatements = makeFinancialStatements(
-                FinancialStatements(), historicalFinancialStatements
-            )
-
-            # merge the latest
-            financialStatements = makeFinancialStatements(
-                financialStatements, latestFinancialStatements
-            )
-
-        else:
-            financialStatements = makeFinancialStatements(
-                stock.financialStatements, latestFinancialStatements
-            )
-
-        # if empty financial statements, we don't want to save it
-        if not financialStatements:
-            print("No financial statements.")
-            removeStock(stockRef, symbol)
-
-            if targetSymbol:
-                break
-
-            continue
-
-        stock.financialStatements = financialStatements
-
-        # get new historical pricing
-        historicalPricing = fetchHistoricalPricing(symbol)
-
-        if not historicalPricing:
-            print("No historical pricing.")
-            removeStock(stockRef, symbol)
-
-            if targetSymbol:
-                break
-
-            continue
-
-        if historicalPricing:
-            stock.historicalPricing = historicalPricing
-
-        # get the latest dividends
-        stock = handleDividendsPaid(stock)
-
-        # evaluate the stock
-        stock.valuation = evaluate(stock)
-
-        # add the last updated date
-        stock.lastUpdated = today
-
-        # convert our stock class to a json string
-        stockJson = json.loads(
-            json.dumps(stock, default=lambda o: o.__dict__, indent=2)
-        )
-
-        stockRef.set(stockJson, merge=True)
-
-        if freshy:
-            # store the data locally
-            filepath = f"data/stocks/{exchange}/{symbol}.json"
-            with safeOpenWrite(filepath) as file:
-                jsonString = json.dumps(
-                    stockJson, default=lambda o: o.__dict__, indent=2
-                )
-                file.write(jsonString)
-
-        print(
-            f"{symbol} is {stock.valuation.health}. You should {stock.valuation.instruction}. You can expected a return of {stock.valuation.expectedReturn}%. The current price is {stock.currentPrice} and we value the stock at {stock.valuation.fairValue}."
-        )
-
-        if targetSymbol:
-            break
+        processStock(symbol, isLoop)
